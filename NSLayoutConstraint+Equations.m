b188b40188fc1fb863c72f41c29310901e61743d
Vladimir Kirillov <proger@wilab.org.ua>
2013-05-17 17:08:50 +0300

    recognize subtraction operator. now you can do this:
        [button constrain:@"right = right - 10" to:view];
        [button constrain:@"bottom = bottom - 10" to:view];

--- NSLayoutConstraint+Equations.m
+++ NSLayoutConstraint+Equations.m
@@ -15,31 +15,31 @@
     //parse the formula
     //the format is property { = | < | > } [multiplier *] property [+ constant]
     //or if RHS is nil property { = | < | > } constant
-    
+
     NSString *lhsPropertyString, *rhsPropertyString, *relationString;
     CGFloat multiplier = 1.0, constant = 0.0;
-    
+
     static NSRegularExpression* expr = nil;
     static NSRegularExpression* constExpr = nil;
     static NSDictionary* layoutDict;
     static NSDictionary* relationDict;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        NSString* pattern = @"([a-zA-Z_][a-zA-Z0-9_]*)\\s*(=|<|>)\\s*(?:(\\d+(?:\\.\\d+)?)\\s*\\*)?\\s*([a-zA-Z_][a-zA-Z0-9_]+)\\s*(?:\\+\\s*(\\d+(?:\\.\\d+)?))?";
+        NSString* pattern = @"([a-zA-Z_][a-zA-Z0-9_]*)\\s*(=|<|>)\\s*(?:(\\d+(?:\\.\\d+)?)\\s*\\*)?\\s*([a-zA-Z_][a-zA-Z0-9_]+)\\s*(?:([\\+-])\\s*(\\d+(?:\\.\\d+)?))?";
         NSError* err;
         expr = [NSRegularExpression regularExpressionWithPattern:pattern options:0 error:&err];
         if (expr == nil) {
             NSLog(@"%@",err);
             abort();
         }
-        
+
         pattern = @"([a-zA-Z_][a-zA-Z0-9_]*)\\s*(=|<|>)\\s*(\\d+(?:\\.\\d+)?)";
         constExpr = [NSRegularExpression regularExpressionWithPattern:pattern options:0 error:&err];
         if (constExpr == nil) {
             NSLog(@"%@",err);
             abort();
         }
-        
+
         layoutDict = @{
                        @"baseline" : @(NSLayoutAttributeBaseline),
                        @"bottom" : @(NSLayoutAttributeBottom),
@@ -53,25 +53,25 @@
                        @"trailing" : @(NSLayoutAttributeTrailing),
                        @"width" : @(NSLayoutAttributeWidth)
                        };
-        
+
         relationDict = @{
                          @"=" : @(NSLayoutRelationEqual),
                          @"<" : @(NSLayoutRelationLessThanOrEqual),
                          @">" : @(NSLayoutRelationGreaterThanOrEqual)
                          };
     });
-    
-    
+
+
     if (rhs == nil) {
         NSTextCheckingResult* rslt = [constExpr firstMatchInString:formula options:0 range:NSMakeRange(0, formula.length)];
-        
+
         lhsPropertyString = [formula substringWithRange:[rslt rangeAtIndex:1]];
         relationString = [formula substringWithRange:[rslt rangeAtIndex:2]];
         constant = [[formula substringWithRange:[rslt rangeAtIndex:3]] floatValue];
-        
+
         NSLayoutAttribute lhsAttribute = [layoutDict[lhsPropertyString] integerValue];
         NSLayoutRelation relation = [relationDict[relationString] integerValue];
-        
+
         return [self constraintWithItem:lhs
                               attribute:lhsAttribute
                               relatedBy:relation
@@ -80,29 +80,35 @@
                              multiplier:1.0
                                constant:constant];
     } else {
-        
+
         NSTextCheckingResult* rslt = [expr firstMatchInString:formula options:0 range:NSMakeRange(0, formula.length)];
-        
+
         //assign our strings
         lhsPropertyString = [formula substringWithRange:[rslt rangeAtIndex:1]];
         relationString = [formula substringWithRange:[rslt rangeAtIndex:2]];
-        
+
         if ([rslt rangeAtIndex:3].length > 0) {
             multiplier = [[formula substringWithRange:[rslt rangeAtIndex:3]] floatValue];
         }
-        
+
         rhsPropertyString = [formula substringWithRange:[rslt rangeAtIndex:4]];
-        
+
         if ([rslt rangeAtIndex:5].length > 0) {
-            constant = [[formula substringWithRange:[rslt rangeAtIndex:5]] floatValue];
+            NSRange constRange = [rslt rangeAtIndex:6];
+            NSAssert(constRange.length, @"constant missing");
+            constant = [[formula substringWithRange:constRange] floatValue];
+
+            NSString *op = [formula substringWithRange:[rslt rangeAtIndex:5]];
+            if ([op isEqual:@"-"])
+                constant *= -1;
         }
-        
+
         //translate property strings to properties
         NSLayoutAttribute lhsAttribute = [layoutDict[lhsPropertyString] integerValue];
         NSLayoutAttribute rhsAttribute = [layoutDict[rhsPropertyString] integerValue];
-        
+
         NSLayoutRelation relation = [relationDict[relationString] integerValue];
-        
+
         return [self constraintWithItem:lhs
                               attribute:lhsAttribute
                               relatedBy:relation
